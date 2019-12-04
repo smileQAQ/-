e8d1fdc526753b9e0a50235e137458d8e78ab39a
smileQAQ <1585362781@qq.com>
2019-12-04 15:16:35 +0800

    猫眼影城

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,7 +5,97 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    movieImg: Array(4),
+    funcTag: [
+      {
+        imgUrl: 'https://www.websitetomine.top/image/base/01d142a61cca159b2b2b5a7d9ca4a7f5.png',
+        title: '签到必领券'
+      },
+      {
+        imgUrl: 'https://www.websitetomine.top/image/base/01d142a61cca159b2b2b5a7d9ca4a7f5.png',
+        title: '冰雪奇缘2'
+      },
+      {
+        imgUrl: 'https://www.websitetomine.top/image/base/01d142a61cca159b2b2b5a7d9ca4a7f5.png',
+        title: '本周六开唱'
+      },
+      {
+        imgUrl: 'https://www.websitetomine.top/image/base/01d142a61cca159b2b2b5a7d9ca4a7f5.png',
+        title: '杨丞琳抢票'
+      }
+    ],
+    adMovieData:{
+      img: 'https://www.websitetomine.top/image/base/c3484fec3154d9f0620d501baf77d233.jpg',
+      name: '疯狂的外星人',
+      score: 8.5,
+      desc: '评分超越93%的剧情片'
+    },
+    movieList:[
+      {
+        img: 'https://p0.meituan.net/moviemachine/58ee13be6dc60bf5e636cf915bbbaaa55787785.jpg@464w_644h_1e_1c',
+        title: '冰雪奇缘2',
+        score: 8.9,
+        actor: '哈哈哈哈哈哈哈啊哈哈啊哈哈哈啊哈哈啊哈啊哈哈',
+        nearShowTime: '今天262家影院放映2222场',
+        isShow: '1'
+      }, {
+        img: 'https://p0.meituan.net/moviemachine/58ee13be6dc60bf5e636cf915bbbaaa55787785.jpg@464w_644h_1e_1c',
+        title: '冰雪奇缘2',
+        score: 8.9,
+        actor: '哈哈哈哈哈哈哈啊哈哈啊哈哈哈啊哈哈啊哈啊哈哈',
+        nearShowTime: '今天262家影院放映2222场',
+        isShow: '1'
+      }, {
+        img: 'https://p0.meituan.net/moviemachine/58ee13be6dc60bf5e636cf915bbbaaa55787785.jpg@464w_644h_1e_1c',
+        title: '冰雪奇缘2',
+        score: 8.9,
+        actor: '哈哈哈哈哈哈哈啊哈哈啊哈哈哈啊哈哈啊哈啊哈哈',
+        nearShowTime: '今天262家影院放映2222场',
+        isShow: '1'
+      }, {
+        img: 'https://p0.meituan.net/moviemachine/58ee13be6dc60bf5e636cf915bbbaaa55787785.jpg@464w_644h_1e_1c',
+        title: '冰雪奇缘2',
+        score: 8.9,
+        actor: '哈哈哈哈哈哈哈啊哈哈啊哈哈哈啊哈哈啊哈啊哈哈',
+        nearShowTime: '今天262家影院放映2222场',
+        isShow: '1'
+      }, {
+        img: 'https://p0.meituan.net/moviemachine/58ee13be6dc60bf5e636cf915bbbaaa55787785.jpg@464w_644h_1e_1c',
+        title: '冰雪奇缘2',
+        score: 8.9,
+        actor: '哈哈哈哈哈哈哈啊哈哈啊哈哈哈啊哈哈啊哈啊哈哈',
+        nearShowTime: '今天262家影院放映2222场',
+        isShow: '1'
+      }, {
+        img: 'https://p0.meituan.net/moviemachine/58ee13be6dc60bf5e636cf915bbbaaa55787785.jpg@464w_644h_1e_1c',
+        title: '冰雪奇缘2',
+        score: 8.9,
+        actor: '哈哈哈哈哈哈哈啊哈哈啊哈哈哈啊哈哈啊哈啊哈哈',
+        nearShowTime: '今天262家影院放映2222场',
+        isShow: '1'
+      }, {
+        img: 'https://p0.meituan.net/moviemachine/58ee13be6dc60bf5e636cf915bbbaaa55787785.jpg@464w_644h_1e_1c',
+        title: '冰雪奇缘2',
+        score: 8.9,
+        actor: '哈哈哈哈哈哈哈啊哈哈啊哈哈哈啊哈哈啊哈啊哈哈',
+        nearShowTime: '今天262家影院放映2222场',
+        isShow: '1'
+      }, {
+        img: 'https://p0.meituan.net/moviemachine/58ee13be6dc60bf5e636cf915bbbaaa55787785.jpg@464w_644h_1e_1c',
+        title: '冰雪奇缘2',
+        score: 8.9,
+        actor: '哈哈哈哈哈哈哈啊哈哈啊哈哈哈啊哈哈啊哈啊哈哈',
+        nearShowTime: '今天262家影院放映2222场',
+        isShow: '1'
+      }, {
+        img: 'https://p0.meituan.net/moviemachine/58ee13be6dc60bf5e636cf915bbbaaa55787785.jpg@464w_644h_1e_1c',
+        title: '冰雪奇缘2',
+        score: 8.9,
+        actor: '哈哈哈哈哈哈哈啊哈哈啊哈哈哈啊哈哈啊哈啊哈哈',
+        nearShowTime: '今天262家影院放映2222场',
+        isShow: '1'
+      }
+    ]
   },
 
   /**
